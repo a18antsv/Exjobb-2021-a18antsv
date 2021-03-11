cd749653cd69948905f23a91a65991b4d4a94c15
a18antsv <gotneifhd@gmail.com>
2021-03-11 22:10:50 +0900

    Connect consumer service to RabbitMQ

--- rabbitmq/consumer-service/consumer-service.js
+++ rabbitmq/consumer-service/consumer-service.js
@@ -1,1 +1,21 @@
 import amqp from "amqplib";
+
+const QUEUE_NAME = "test-queue";
+
+const amqpConnectionSettings = {
+  protocol: "amqp",
+  hostname: "localhost",
+  port: 5672,
+  username: "guest",
+  password: "guest",
+  vhost: "/",
+};
+
+(async () => {
+  const connection = await amqp.connect(amqpConnectionSettings);
+  const channel = await connection.createChannel();
+
+  // Create queue with given name if it does not already exist
+  await channel.assertQueue(QUEUE_NAME);
+
+})();
