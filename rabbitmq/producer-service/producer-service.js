9e56488de033310a69d53da89bd1af5cb4b3a470
a18antsv <gotneifhd@gmail.com>
2021-03-11 16:25:15 +0900

    Successfully established connection to RabbitMQ

--- rabbitmq/producer-service/producer-service.js
+++ rabbitmq/producer-service/producer-service.js
@@ -1,1 +1,14 @@
 import amqp from "amqplib";
+
+(async () => {
+
+  const connection = await amqp.connect({
+    protocol: "amqp",
+    hostname: "localhost",
+    port: 5672,
+    username: "guest",
+    password: "guest",
+    vhost: "/",
+  });
+
+})();
