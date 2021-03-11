423630db17943568ef069a26de277025f1200a84
a18antsv <gotneifhd@gmail.com>
2021-03-11 16:56:39 +0900

    Created test exchange and text queue

--- rabbitmq/producer-service/producer-service.js
+++ rabbitmq/producer-service/producer-service.js
@@ -1,5 +1,9 @@
 import amqp from "amqplib";
 
+const EXCHANGE_NAME = "test-exchange";
+const EXCHANGE_TYPE = "direct";
+const QUEUE_NAME = "test-queue";
+
 (async () => {
 
   const connection = await amqp.connect({
@@ -12,5 +16,11 @@ import amqp from "amqplib";
   });
 
   const channel = await connection.createChannel();
+
+  // Create exchange with given name if it does not already exist
+  await channel.assertExchange(EXCHANGE_NAME, EXCHANGE_TYPE);
+
+  // Create queue with given name if it does not already exist
+  await channel.assertQueue(QUEUE_NAME);
 
 })();
