3d7446762f68821c916b55717415f9e83e3cb5d7
a18antsv <gotneifhd@gmail.com>
2021-03-11 17:15:03 +0900

    Bind test queue to test exchange with binding key

--- rabbitmq/producer-service/producer-service.js
+++ rabbitmq/producer-service/producer-service.js
@@ -3,6 +3,7 @@ import amqp from "amqplib";
 const EXCHANGE_NAME = "test-exchange";
 const EXCHANGE_TYPE = "direct";
 const QUEUE_NAME = "test-queue";
+const BINDING_KEY = "test-binding";
 
 (async () => {
 
@@ -22,5 +23,8 @@ const QUEUE_NAME = "test-queue";
 
   // Create queue with given name if it does not already exist
   await channel.assertQueue(QUEUE_NAME);
+
+  // Binds queue to exchange according to exchange type and binding key
+  await channel.bindQueue(QUEUE_NAME, EXCHANGE_NAME, BINDING_KEY);
 
 })();
