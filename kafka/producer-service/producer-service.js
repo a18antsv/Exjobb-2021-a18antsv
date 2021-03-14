654852d75db5f0f4155806ed11baf66f21352c31
a18antsv <gotneifhd@gmail.com>
2021-03-15 00:20:11 +0900

    Connect producer service to Kafka

--- kafka/producer-service/producer-service.js
+++ kafka/producer-service/producer-service.js
@@ -1,1 +1,14 @@
 import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "producer-service-1",
+  brokers: ["localhost:9092"]
+});
+
+const producer = kafka.producer();
+
+(async () => {
+  
+  await producer.connect();
+
+})();
