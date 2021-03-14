6d1d3f7a06e2798bcf9a9c142cf10e557c56704b
a18antsv <gotneifhd@gmail.com>
2021-03-15 02:08:56 +0900

    Add comments Kafka consumer service

--- kafka/consumer-service/consumer-service.js
+++ kafka/consumer-service/consumer-service.js
@@ -5,6 +5,7 @@ const kafka = new Kafka({
   brokers: ["localhost:9092"]
 });
 
+// Create a consumer that joins a consumer group (required)
 const consumer = kafka.consumer({
   groupId: "test-consumer-group"
 });
@@ -12,11 +13,13 @@ const consumer = kafka.consumer({
 (async () => {
   await consumer.connect();
 
+  // Subscribe consumer group to topic and start using latest offset
   await consumer.subscribe({
     topic: "test-topic",
     fromBeginning: false
   });
   
+  // Run consumer and handle one message at a time
   consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       console.log(`
