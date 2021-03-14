745034b08be43b9327395ff5587e274de4f367c3
a18antsv <gotneifhd@gmail.com>
2021-03-15 01:01:42 +0900

    Send test message to Kafka

--- kafka/producer-service/producer-service.js
+++ kafka/producer-service/producer-service.js
@@ -1,4 +1,10 @@
-import { Kafka } from "kafkajs";
+import { CompressionTypes, Kafka } from "kafkajs";
+
+const TOPIC_NAME = "test-topic";
+const MESSAGE_OBJECT = {
+  key: "test-key",
+  value: "test-message",
+};
 
 const kafka = new Kafka({
   clientId: "producer-service-1",
@@ -10,5 +16,13 @@ const producer = kafka.producer();
 (async () => {
   
   await producer.connect();
+
+  const recordMetadata = await producer.send({
+    topic: TOPIC_NAME,
+    messages: [ MESSAGE_OBJECT ],
+    acks: -1, // 0 = no acks, 1 = Only leader, -1 = All insync replicas
+    timeout: 30000,
+    compression: CompressionTypes.None,
+  });
 
 })();
