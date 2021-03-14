9882fe7ca7be8b94d5975a4e8fc8950d3c153974
a18antsv <gotneifhd@gmail.com>
2021-03-15 01:35:19 +0900

    Connect consumer service to Kafka

--- kafka/consumer-service/consumer-service.js
+++ kafka/consumer-service/consumer-service.js
@@ -1,1 +1,15 @@
 import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "consumer-service-1",
+  brokers: ["localhost:9092"]
+});
+
+const consumer = kafka.consumer({
+  groupId: "test-consumer-group"
+});
+
+(async () => {
+  await consumer.connect();
+
+})();
