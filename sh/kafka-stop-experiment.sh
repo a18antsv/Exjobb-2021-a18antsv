bda9c0228ce00378f8043a6997fef8cf0b6d46d9
a18antsv <gotneifhd@gmail.com>
2021-04-05 18:56:08 +0200

    Remove a variable number of producers in script

--- sh/kafka-stop-experiment.sh
+++ sh/kafka-stop-experiment.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Forecefully remove all containers related to a Kafka experiement
+# Remove all producer containers by force
+for ((i = 1; i <= $PRODUCERS; i++))
+do
+  docker rm -f kafka-producer-service-$i
+done
+
+# Forecefully remove the rest of the containers related to a Kafka experiement
 # Another option would be to stop them first and then remove them
-docker rm -f kafka-producer-service-1
 docker rm -f kafka-consumer-service-1
 docker rm -f kafka-broker-1
 docker rm -f zookeeper-node-1
