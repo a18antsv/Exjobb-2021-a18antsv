d241756fdd1b31f48fc787d5c9c4512fa78a9eb3
a18antsv <gotneifhd@gmail.com>
2021-04-05 20:01:03 +0200

    Server code to get experiment by id and set experiment status

--- dashboard/app.js
+++ dashboard/app.js
@@ -3,6 +3,28 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Statuses that an experiment can have
+const Status = {
+  NOT_STARTED: "Not started",
+  IN_QUEUE: "In queue",
+  IN_PROGRESS: "In progress",
+  COMPLETED: "Completed"
+};
+
+let experiments = []; // All created experiments (whole JSON-objects)
+
+const getExperimentById = experimentId => {
+  return experiments.find(experiment => experiment.experimentId === experimentId);
+}
+
+const setExperimentStatus = (experimentId, status) => {
+  const experiment = getExperimentById(experimentId);
+  if(!experiment) {
+    console.log(`Could not find experiment with id ${experimentId}`);
+    return;
+  }
+  experiment.status = status;
+}
 app.use("/", express.static("./public"));
 
 app.listen(port, () => {
