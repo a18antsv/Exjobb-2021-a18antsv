7761e7df9855bf789aab6db9a59e306463484019
a18antsv <gotneifhd@gmail.com>
2021-04-06 02:31:25 +0200

    Remove duplicated remove of rabbimq producer container

--- sh/rabbitmq-stop-experiment.sh
+++ sh/rabbitmq-stop-experiment.sh
@@ -10,6 +10,5 @@ done
 
 # Forecefully remove the rest of the containers related to a RabbitMQ experiment
 # Another option would be to stop them first and then remove them
-docker rm -f rabbitmq-producer-service-1
 docker rm -f rabbitmq-consumer-service-1
 docker rm -f rabbit-node-1
